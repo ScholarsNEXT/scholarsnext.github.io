9f8df05151849b83dd7c6f0edeb4ae3267142522
Albert Huang <alberthdev@users.noreply.github.com>
2016-01-19 04:56:34 -0500

    Fix initial location for map

--- js/autoComplete.js
+++ js/autoComplete.js
@@ -1,6 +1,6 @@
 function initAutocomplete() {
   var map = new google.maps.Map(document.getElementById('hotelmap'), {
-    center: {lat: -38.9875, lng: 76.9400},
+    center: {lat: 38.9875, lng: -76.9400},
     zoom: 10,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
